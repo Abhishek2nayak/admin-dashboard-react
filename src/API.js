8663ca0393b7c528a7032da625aaf903dac6eb15
abhishek nayak <abhishek5nayak22@gmail.com>
2023-12-02 21:20:44 +0530

    updated

--- src/API.js
+++ src/API.js
@@ -1,13 +1,13 @@
-import React from 'react';
+
 
 export async function getMembers() {
     const res = await fetch("https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json")
     if (!res.ok) {
-        throw {
+        throw new Error({
             message: "Failed to fetch Members", 
             statusText: res.statusText,
             status: res.status
-        }
+        })
     }
     const data = await res.json();
     return data;
